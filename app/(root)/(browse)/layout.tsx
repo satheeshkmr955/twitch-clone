645696e1c81c4ea6e0dd5fe7c6042fcd22660de0
Satheeshkumar P <satheesh@trendlyne.com>
2024-02-25 18:33:56 +0530

    added sidebar

--- app/(root)/(browse)/layout.tsx
+++ app/(root)/(browse)/layout.tsx
@@ -1,4 +1,6 @@
 import { Navbar } from "./_components/navbar";
+import { Container } from "./_components/container";
+import { Sidebar } from "./_components/sidebar";
 
 const BrowseLayout = ({
   children,
@@ -8,7 +10,10 @@ const BrowseLayout = ({
   return (
     <>
       <Navbar />
-      <div className="flex h-full pt-20">{children}</div>
+      <div className="flex h-full pt-20">
+        <Sidebar />
+        <Container>{children}</Container>
+      </div>
     </>
   );
 };
