5aa8a4f9e1524c5e3042e051d8c5f8eaae4b2789
Satheeshkumar P <satheesh@trendlyne.com>
2024-03-23 23:59:39 +0530

    completed header component

---
+++ components/stream-player/actions.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import { HeartIcon } from "lucide-react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+
+import { SIGN_IN } from "@/constants/route.constants";
+import { getQueryClient } from "@/lib/queryclient";
+import { cn, triggerToast } from "@/lib/utils";
+import { getCacheKey, useMutationGraphQL } from "@/hooks/use-graphql";
+import {
+  FollowUserDocument,
+  GetFollowedAndRecommendedUserDocument,
+  GetUserByNameWithAllDetailsDocument,
+  UnFollowUserDocument,
+} from "@/gql/graphql";
+import { TriggerToastProps } from "@/app/_types";
+
+interface ActionsProps {
+  hostIdentity: string;
+  isFollowing: boolean;
+  isHost: boolean;
+}
+
+export const Actions = (props: ActionsProps) => {
+  const { isFollowing, isHost, hostIdentity } = props;
+
+  const router = useRouter();
+  const { data: session } = useSession();
+  const userId = session?.user?.id || "";
+
+  const { mutate: followMutate, isPending: followPending } = useMutationGraphQL(
+    FollowUserDocument,
+    {
+      input: { id: userId },
+    },
+    {
+      onSuccess: (data) => {
+        triggerToast(data.data?.followUser?.toast! as TriggerToastProps);
+        if (data.data?.followUser?.follow) {
+          const queryKey1 = [
+            getCacheKey(GetUserByNameWithAllDetailsDocument),
+            {
+              input: { name: data.data.followUser.follow.following.slugName },
+            },
+          ];
+          const queryKey2 = [
+            getCacheKey(GetFollowedAndRecommendedUserDocument),
+          ];
+          const queryClient = getQueryClient();
+          queryClient.invalidateQueries({ queryKey: queryKey1 });
+          queryClient.invalidateQueries({ queryKey: queryKey2 });
+        }
+      },
+      onError(error) {
+        // console.log("error", error);
+      },
+    }
+  );
+
+  const { mutate: unFollowMutate, isPending: unFPending } = useMutationGraphQL(
+    UnFollowUserDocument,
+    {
+      input: { id: userId },
+    },
+    {
+      onSuccess: (data) => {
+        triggerToast(data.data?.unFollowUser?.toast! as TriggerToastProps);
+        if (data.data?.unFollowUser?.follow) {
+          const queryKey1 = [
+            getCacheKey(GetUserByNameWithAllDetailsDocument),
+            {
+              input: { name: data.data.unFollowUser.follow.following.slugName },
+            },
+          ];
+          const queryKey2 = [
+            getCacheKey(GetFollowedAndRecommendedUserDocument),
+          ];
+          const queryClient = getQueryClient();
+          queryClient.invalidateQueries({ queryKey: queryKey1 });
+          queryClient.invalidateQueries({ queryKey: queryKey2 });
+        }
+      },
+      onError(error) {
+        // console.log("error", error);
+      },
+    }
+  );
+
+  const toggleFollow = () => {
+    if (!userId) {
+      return router.push(SIGN_IN);
+    }
+
+    if (isHost) return;
+
+    if (isFollowing) {
+      unFollowMutate();
+    } else {
+      followMutate();
+    }
+  };
+
+  const isPending = followPending || unFPending;
+
+  return (
+    <Button
+      onClick={toggleFollow}
+      disabled={isPending || isHost}
+      variant="primary"
+      size="sm"
+      className="w-full lg:w-auto"
+    >
+      <HeartIcon
+        className={cn("h-4 w-4 mr-2", isFollowing ? "fill-white" : "fill-none")}
+      />
+
+      {isFollowing ? "Unfollow" : "Follow"}
+    </Button>
+  );
+};
+
+export const ActionsSkeleton = () => {
+  return <Skeleton className="h-10 w-full lg:w-24" />;
+};
+
+Actions.Skeleton = ActionsSkeleton;
--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -6,6 +6,7 @@ import { Stream, User } from "@/gql/graphql";
 import { Video, VideoSkeleton } from "@/components/stream-player/video";
 import { Chat, ChatSkeleton } from "@/components/stream-player/chat";
 import { ChatToggle } from "@/components/stream-player/chat-toggle";
+import { Header, HeaderSkeleton } from "@/components/stream-player/header";
 
 import { cn } from "@/lib/utils";
 import { useChatSidebar } from "@/store/use-chat-sidebar";
@@ -44,6 +45,14 @@ export const StreamPlayer = (props: StreamPlayerProps) => {
       >
         <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
           <Video hostName={user.name} hostIdentity={user.id} />
+          <Header
+            hostName={user.name}
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            imageUrl={user.image || null}
+            isFollowing={isFollowing}
+            name={stream.name}
+          />
         </div>
         <div className={cn("col-span-1", collapsed && "hidden")}>
           <Chat
@@ -66,6 +75,7 @@ export const StreamPlayerSkeleton = () => {
     <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
       <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
         <VideoSkeleton />
+        <HeaderSkeleton />
       </div>
       <div className="col-span-1 bg-background">
         <ChatSkeleton />
